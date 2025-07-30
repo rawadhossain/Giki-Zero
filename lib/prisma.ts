51bcfd456e22ffdcead25c01863a4fa1436f9f80
Rawad Hossain <rawad.hossain00@gmail.com>
2025-07-30 23:25:29 +0600

    prisma client singleton fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,14 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+const prismaClientSingleton = () => {
+	return new PrismaClient();
+};
+
+declare global {
+	// eslint-disable-next-line no-var
+	var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
